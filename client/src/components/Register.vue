79c63f71e4dd44a6a824ba500dea882d78becbc7
Alexander Hamilton <alexander0hamilton@gmail.com>
2024-07-18 18:19:13 -0400

    stuff

---
+++ client/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input 
+      type="email" 
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input 
+      type="passoword" 
+      name="passoword"
+      v-model="password"
+      placeholder="passoword" />
+    <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email:'',
+      password:''
+    }
+  },
+  methods: {
+    async register () {    
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log()
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
